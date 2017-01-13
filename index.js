5292f02767393de0903e2264b7a7e1a75c6fed3a
wuhy <sparklewhy@gmail.com>
2017-01-13 20:47:32 +0800

    add remove css require option

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 'use strict';
 
 var amdWrapper = require('amd-wrapper');
+var url = require('url');
 
 var amdRequireConfig = null;
 function getAMDRequireConfig() {
@@ -15,17 +16,51 @@ function getAMDRequireConfig() {
     return amdRequireConfig;
 }
 
+var requireRegExp = /\s*require\s*\(\s*(['"])([^'"]+)\1\s*\)\s*;?/g;
+var styleFileRegExp = /\.(css|styl|sass|less)($|\?)/;
+
+function clearCssRequire(content, file) {
+    return content.replace(
+        requireRegExp,
+        function (match, quot, id) {
+            if (!styleFileRegExp.test(id)) {
+                return match;
+            }
+
+            var styleUrl = url.parse(id);
+            var info = fis.project.lookup(styleUrl.pathname, file);
+            if (!info.file || !info.file.isCssLike) {
+                return match;
+            }
+
+            file.addRequire(info.id);
+            if (info.file.isFile()) {
+                file.addLink(info.file.subpath);
+            }
+
+            return '';
+        }
+    );
+}
+
 function amdWrap(content, file, conf) {
     if (file.disableWrap) {
         return content;
     }
-    return amdWrapper(content, Object.assign({
+
+    var result = amdWrapper(content, Object.assign({
         filePath: file.subpath.substr(1),
         requireConfig: getAMDRequireConfig,
         projectRoot: fis.project.getProjectPath(),
         componentDirName: fis.getDepDirName(),
         checkUMD: true
     }, conf || {}));
+
+    if (conf.clearCssRequire) {
+        result = clearCssRequire(result, file);
+    }
+
+    return result;
 }
 
 module.exports = exports = amdWrap;
